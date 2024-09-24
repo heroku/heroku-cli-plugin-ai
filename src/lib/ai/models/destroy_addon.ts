4c4949507ff525792e18ece03af6ab071ddbef63
Zane Whitfield <zwhitfield@salesforce.com>
2024-09-24 09:14:25 -0700

    WIP add destroy_addon util

---
+++ src/lib/ai/models/destroy_addon.ts
@@ -0,0 +1,21 @@
+import color from '@heroku-cli/color'
+import {ux} from '@oclif/core'
+import {APIClient} from '@heroku-cli/command'
+import * as Heroku from '@heroku-cli/schema'
+
+export default async function (heroku: APIClient, addon: Heroku.AddOn, force = false) {
+  const addonName = addon.name || ''
+
+  ux.action.start(`Destroying ${color.addon(addonName)} in the background.\n The app will restart when complete...`)
+
+  const {body: addonDelete} = await heroku.delete<Heroku.AddOn>(`/apps/${addon.app?.id}/addons/${addon.id}`, {
+    headers: {'Accept-Expansion': 'plan'},
+    body: {force},
+  }).catch(error => {
+    const error_ = error.body && error.body.message ? new Error(`The add-on was unable to be destroyed: ${error.body.message}.`) : new Error(`The add-on was unable to be destroyed: ${error}.`)
+    throw error_
+  })
+
+  ux.action.stop()
+  return addonDelete
+}
