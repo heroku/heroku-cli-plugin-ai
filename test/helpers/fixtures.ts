f95a05ed260f93800c29c7fab46dfd2fd8644599
Zane Whitfield <zwhitfield@salesforce.com>
2024-09-25 14:36:51 -0700

    Update mockConfigVars

--- test/helpers/fixtures.ts
+++ test/helpers/fixtures.ts
@@ -31,10 +31,18 @@ export const availableModels = [
   },
 ]
 
-export const mockConfigVars = {
-  INFERENCE_KEY: 's3cr3t_k3y',
-  INFERENCE_MODEL_ID: 'claude-3-5-sonnet',
-  INFERENCE_URL: 'inference-eu.heroku.com',
+export const configureMockConfigVars = (addonAttachment: Heroku.AddOnAttachment) => {
+  const updatedMockConfigVars: any = {}
+
+  const newInferenceKey = `${addonAttachment.addon?.name.toUpperCase()}_KEY`
+  const newInferenceURL = `${addonAttachment.addon?.name.toUpperCase()}_URL`
+  const newInferenceMODELID = `${addonAttachment.addon?.name.toUpperCase()}_MODEL_ID`
+
+  updatedMockConfigVars[newInferenceKey] = 's3cr3t_k3y'
+  updatedMockConfigVars[newInferenceMODELID] = 'claude-3-5-sonnet'
+  updatedMockConfigVars[newInferenceURL] = 'inference-eu.heroku.com'
+
+  return updatedMockConfigVars
 }
 
 export const mockAPIErrors = {
