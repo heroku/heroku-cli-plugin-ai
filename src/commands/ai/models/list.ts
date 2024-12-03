9f9ba35da35481bd628718797e01b08081a1b360
Zane Whitfield <zwhitfield@salesforce.com>
2024-12-02 22:18:45 -0800

    Add individual model card urls per model

--- src/commands/ai/models/list.ts
+++ src/commands/ai/models/list.ts
@@ -11,8 +11,22 @@ const displayModels = (models: ModelList) => {
     types: {
       get: ({type}: any) => type.join(', '),
     },
-  }, {'no-header': true})
+    model_card_links: {
+      get: ({link}: any) => link,
+    },
+  }, {'no-header': false})
 }
+
+// This is a temporary hardcoding of model cards
+// until the AI API can support these links
+const modelCardURLs = [
+  'https://devcenter.heroku.com/articles/heroku-inference_model-cards_claude-3-5-haiku',
+  'https://devcenter.heroku.com/articles/heroku-inference_model-cards_claude-3-5-sonnet',
+  'https://devcenter.heroku.com/articles/heroku-inference_model-cards_claude-3-5-sonnet-latest',
+  'https://devcenter.heroku.com/articles/heroku-inference_model-cards_claude-3-haiku',
+  'https://devcenter.heroku.com/articles/heroku-inference_model-cards_cohere-embed-multilingual',
+  'https://devcenter.heroku.com/articles/heroku-inference_model-cards_stable-image-ultra',
+]
 
 export default class List extends Command {
   static description = 'list available AI models to provision access to'
@@ -30,6 +44,13 @@ export default class List extends Command {
     const urlPath = '/available-models'
 
     const {body: availableModels} = await herokuAIClient.get<ModelList>(urlPath)
+
+    // This is a temporary hardcoding of model cards
+    // to model names until the AI API can support these links
+    for (const [index, availableModel] of availableModels.entries()) {
+      availableModel.link = modelCardURLs[index]
+    }
+
     displayModels(availableModels)
     ux.log('\nSee https://devcenter.heroku.com/articles/heroku-inference_model-cards for more info.')
   }
