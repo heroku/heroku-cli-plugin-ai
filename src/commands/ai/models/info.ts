b76c82b04d352befc8095b864e6b4b8e7fc8b441
Justin Wilaby <jwilaby@gmail.com>
2025-03-10 08:41:47 -0700

    Revert/k80/models info arg required (#70)
    * Revert "refactor: make model_resource arg required for ai:models:info"
    
    This reverts commit 497581fa2f15c5b6416935d32f8f5e5a1ba32b8e.
    
    * Revert "docs: update help and error text for ai:models:info"
    
    This reverts commit 88249e03657a5406a4c5dd3fac968a185fc75fd7.
    
    * Revert "test: remove tests for calling ai:models:info without required arg"
    
    This reverts commit 7aae796130888b4f82a65061c44e10ba507b8738.
    
    * Revert "refactor: update ai:models:info to work with required model resource arg"
    
    This reverts commit b7be3cf74bf0f07c9972bed5e86c200672a54598.
    
    * Adjusted correctness of README
    
    * Removed model_resource as a required flag
    
    * Revert "fix: update not found error message in base class and tests"
    
    This reverts commit f192933e9a6b1779d856c019128c3718cea9040f.

--- src/commands/ai/models/info.ts
+++ src/commands/ai/models/info.ts
@@ -3,11 +3,14 @@ import {flags} from '@heroku-cli/command'
 import {Args, ux} from '@oclif/core'
 import Command from '../../../lib/base'
 import {ModelResource} from '../../../lib/ai/types'
+import appAddons from '../../../lib/ai/models/app_addons'
+import * as Heroku from '@heroku-cli/schema'
 
 export default class Info extends Command {
-  static description = 'get the current status of an AI model resource attached to your app'
+  static description = 'get the current status of all the AI model resources attached to your app or a specific resource'
   static examples = [
     'heroku ai:models:info claude-3-5-sonnet-acute-04281 --app example-app',
+    'heroku ai:models:info --app example-app',
   ]
 
   static flags = {
@@ -16,48 +19,95 @@ export default class Info extends Command {
   }
 
   static args = {
-    model_resource: Args.string({description: 'resource ID or alias of the model resource', required: true}),
+    model_resource: Args.string({description: 'resource ID or alias of the model resource'}),
   }
 
   public async run(): Promise<any> {
     const {args, flags} = await this.parse(Info)
     const {app} = flags
     const {model_resource: modelResource} = args
+    const synthesizedModels: Array<ModelResource> = []
+    let listOfProvisionedModels: Array<ModelResource>  = []
 
     const modelInfo = async () => {
-      return this.herokuAI.get<ModelResource>(`/models/${this.apiModelId}`, {
+      const modelInfoResponse = await this.herokuAI.get<ModelResource>(`/models/${this.apiModelId}`, {
         headers: {authorization: `Bearer ${this.apiKey}`},
       })
         .catch(error => {
           if (error.statusCode === 404) {
-            ux.warn(`We can’t find a model resource called ${color.addon(modelResource)}.\nRun ${color.cmd('heroku addons -a <app>')} to see a list of model resources.`)
+            ux.warn(`We can’t find a model resource called ${color.yellow(modelResource)}.\nRun ${color.cmd('heroku ai:models:info -a <app>')} to see a list of model resources.`)
           } else {
             throw error
           }
         })
+
+      return modelInfoResponse
     }
 
     const addModelProperties = (alias: string, resourceId: string, modelResource: ModelResource = {} as ModelResource) => {
       return Object.assign(modelResource, {model_alias: alias, model_resource_id: resourceId})
     }
 
-    await this.configureHerokuAIClient(modelResource, app as string)
-    let {body: currentModelResource} = await modelInfo() ?? {body: {} as ModelResource}
-    currentModelResource = addModelProperties(this.modelAlias, this.addonResourceId, currentModelResource)
-    this.displayModelResource(currentModelResource)
+    const getModelDetails = async (collectedModels: Array<Heroku.AddOn> | string) => {
+      if (typeof collectedModels === 'string') {
+        const modelResource = collectedModels
+        await this.configureHerokuAIClient(modelResource, app)
+
+        let {body: currentModelResource} = await modelInfo() ?? {body: {} as ModelResource}
+        currentModelResource = addModelProperties(this.modelAlias, this.addonResourceId, currentModelResource)
+        synthesizedModels.push(currentModelResource!)
+      } else {
+        for (const addonModel of collectedModels) {
+          await this.configureHerokuAIClient(addonModel.modelResource, app)
+
+          let {body: currentModelResource} = await modelInfo() ?? {body: {} as ModelResource}
+          currentModelResource = addModelProperties(this.modelAlias, this.addonResourceId, currentModelResource)
+          synthesizedModels.push(currentModelResource!)
+        }
+      }
+
+      return synthesizedModels
+    }
+
+    if (modelResource) {
+      listOfProvisionedModels = await getModelDetails(modelResource)
+    } else {
+      const provisionedModelsInfo: Record<string, string | undefined>[] = []
+      const inferenceRegex = /inference/
+      const addonsResponse = await appAddons(this.config, app)
+
+      for (const addonInfo of addonsResponse as Array<Heroku.AddOn>) {
+        const addonType = addonInfo.addon_service?.name || ''
+        const isModelAddon = inferenceRegex.test(addonType)
+
+        if (isModelAddon) {
+          provisionedModelsInfo.push({
+            addonName: addonInfo.addon_service?.name,
+            modelResource: addonInfo.name,
+            modelId: addonInfo.addon_service?.id,
+          })
+        }
+      }
+
+      listOfProvisionedModels = await getModelDetails(provisionedModelsInfo)
+    }
+
+    this.displayModelResource(listOfProvisionedModels)
   }
 
-  displayModelResource(modelResource: ModelResource) {
-    ux.log()
-    ux.styledHeader(modelResource.model_id)
-    ux.styledObject({
-      'Base Model ID': modelResource.model_id,
-      'Model Alias': modelResource.model_alias,
-      'Model Resource ID': modelResource.model_resource_id,
-      Ready: modelResource.ready,
-      'Tokens In': modelResource.tokens_in,
-      'Tokens Out': modelResource.tokens_out,
-      'Avg Performance': modelResource.avg_performance,
-    })
+  displayModelResource(modelResources: ModelResource[]) {
+    for (const modelResource of modelResources) {
+      ux.log()
+      ux.styledHeader(modelResource.model_id)
+      ux.styledObject({
+        'Base Model ID': modelResource.model_id,
+        'Model Alias': modelResource.model_alias,
+        'Model Resource ID': modelResource.model_resource_id,
+        Ready: modelResource.ready,
+        'Tokens In': modelResource.tokens_in,
+        'Tokens Out': modelResource.tokens_out,
+        'Avg Performance': modelResource.avg_performance,
+      })
+    }
   }
 }
