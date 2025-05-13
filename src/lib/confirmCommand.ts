554ad33321ba58cdc462737b0b9c38c4b2f08a42
Justin Wilaby <jwilaby@gmail.com>
2025-05-13 08:13:15 -0700

    Update src/lib/confirmCommand.ts
    Co-authored-by: Claire Riley <claire.riley@salesforce.com>
    Signed-off-by: Justin Wilaby <jwilaby@gmail.com>

--- src/lib/confirmCommand.ts
+++ src/lib/confirmCommand.ts
@@ -18,7 +18,7 @@ export default async function confirmCommand(app: string, confirm?: string | und
   ux.warn(message)
   console.error()
   const entered = await ux.prompt(
-    `To proceed, type ${color.bold.red(app)} or re-run this command with ${color.bold.red('--confirm', app)} `,
+    `To proceed, type ${color.bold.red(app)} or re-run this command with ${color.bold.red('--confirm', app)}.`,
     {required: true},
   )
   if (entered === app) {
